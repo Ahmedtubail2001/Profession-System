a2df9b6c4b5f2b2bf6ab2881eb2b81f19a705228
Ahme Tubail <ahmed.tubail2000@gmail.com>
2023-02-07 16:04:26 +0300

    =first commit

---
+++ resources/views/cms/main.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.parent')
+
+@section('title', 'Dashboard')
+@section('page-title', 'Dashboard')
+@section('small-title', 'dashboard')
+
+@section('content')
+
+@endsection
---
+++ resources/views/includes/AppJS.blade.php
@@ -0,0 +1,34 @@
+<!-- jQuery -->
+<script src="{{asset('ass/plugins/jquery/jquery.min.js')}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{asset('ass/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('ass/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- ChartJS -->
+<script src="{{asset('ass/plugins/chart.js/Chart.min.js')}}"></script>
+<!-- Sparkline -->
+<script src="{{asset('ass/plugins/sparklines/sparkline.js')}}"></script>
+<!-- JQVMap -->
+<script src="{{asset('ass/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{asset('ass/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{asset('ass/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<!-- daterangepicker -->
+<script src="{{asset('ass/plugins/moment/moment.min.js')}}"></script>
+<script src="{{asset('ass/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{asset('ass/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<!-- Summernote -->
+<script src="{{asset('ass/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{asset('ass/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('ass/dist/js/adminlte.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('ass/dist/js/demo.js')}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('ass/dist/js/pages/dashboard.js')}}"></script>
